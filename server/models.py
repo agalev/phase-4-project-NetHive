324832d9abce15d9f10467de2d942589383181cd
sdbarlow <sethdbarlow@gmail.com>
2023-04-14 15:06:44 -0600

    added validates

--- server/models.py
+++ server/models.py
@@ -1,6 +1,8 @@
+from wtforms.validators import Email, Regexp
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
+import re
 
 from config import bcrypt, db
 
@@ -17,7 +19,7 @@ class User(db.Model, SerializerMixin):
     image = db.Column(db.String)
     is_online = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
     rooms = db.relationship('RoomUser', backref='user', cascade='all, delete, delete-orphan')
 
@@ -27,6 +29,14 @@ class User(db.Model, SerializerMixin):
 
     @password_hash.setter
     def password_hash(self, password):
+        if len(password) < 8:
+            raise ValueError('Password must be at least 8 characters long')
+        if not re.search('[A-Z]', password):
+            raise ValueError('Password must contain at least one uppercase letter')
+        if not re.search('[a-z]', password):
+            raise ValueError('Password must contain at least one lowercase letter')
+        if not re.search('[0-9]', password):
+            raise ValueError('Password must contain at least one digit')
         password_hash = bcrypt.generate_password_hash(
             password.encode('utf-8'))
         self._password_hash = password_hash.decode('utf-8')
@@ -34,6 +44,21 @@ class User(db.Model, SerializerMixin):
     def authenticate(self, password):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8'))
+    
+    @validates('email')
+    def validate_email(self, key, email):
+        Email()(None, email)
+        existing_user = User.query.filter(User.email == email).first()
+        if existing_user and existing_user.id != self.id:
+            raise ValueError('Email address already registered')
+        return email
+    
+    @validates('first_name', 'last_name')
+    def validate_name(self, key, name):
+        Regexp('^[A-Za-z ]+$', message='Name can only contain letters')(None, name)
+        return name
+    
+
 
 class Conversation(db.Model, SerializerMixin):
     __tablename__ = 'conversations'
@@ -61,6 +86,12 @@ class Room(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     users = db.relationship('RoomUser', backref='room', cascade='all, delete, delete-orphan')
+
+    @validates('topic')
+    def validate_topic(self, key, topic):
+        if len(topic) < 3 or len(topic) > 20:
+            raise ValueError('Topic must be between 3 and 20 characters long.')
+        return topic
 
 class RoomUser(db.Model, SerializerMixin):
     __tablename__ = 'room_users'
