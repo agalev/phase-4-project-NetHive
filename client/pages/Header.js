ff6ae12e9876556f03cae256ac7b5af9f35faa3a
sdbarlow <sethdbarlow@gmail.com>
2023-04-17 11:53:05 -0600

    finishing touches on styling

--- client/pages/Header.js
+++ client/pages/Header.js
@@ -23,9 +23,10 @@ function Header() {
         <span id='home-button' className="ml-2">Back to Home</span>
       </button>
       <div className="flex items-center">
-        <h1 className="text-xl font-large">NetHive</h1>
+        <h1 className="text-4xl font-large text-blue-600">Net</h1>
+        <h1 className="text-4xl font-large text-purple-600 m1-2">Hive</h1>
       </div>
-      <div onClick={handleClick} className="flex items-center w-10 h-10">
+      <div onClick={handleClick} className="flex items-center w-10 h-10 cursor-pointer hover:opacity-75">
         <img id='profile-button' className="w-full h-full rounded-full object-cover" style={{objectFit: "cover"}} src="https://media.wired.com/photos/5f87340d114b38fa1f8339f9/master/w_1600%2Cc_limit/Ideas_Surprised_Pikachu_HD.jpg" alt="Profile" />
       </div>
     </div>
--- client/pages/ProfilePage.js
+++ client/pages/ProfilePage.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from 'react';
+import { useRouter } from 'next/router';
 
 function ProfilePage() {
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      // Replace this with your API call to delete the user
+      console.log('Deleting user...');
+      router.push('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>ProfilePage</div>
-  )
+    <div className="bg-gray-100 min-h-screen">
+      <div className="max-w-2xl mx-auto py-8">
+        <a onClick={() => router.back()} className="text-gray-500 hover:text-gray-700 cursor-pointer">
+          Back
+        </a>
+        <h2 className="text-2xl font-bold mt-4">Account Details</h2>
+        <div className="mt-8 space-y-4">
+          <div>
+            <label className="font-bold">First Name:</label>
+            <span className="ml-2">John</span>
+          </div>
+          <div>
+            <label className="font-bold">Last Name:</label>
+            <span className="ml-2">Doe</span>
+          </div>
+          <div>
+            <label className="font-bold">Email:</label>
+            <span className="ml-2">johndoe@example.com</span>
+          </div>
+          <div>
+            <label className="font-bold">Profile Image:</label>
+            <img
+              src="https://media.wired.com/photos/5f87340d114b38fa1f8339f9/master/w_1600%2Cc_limit/Ideas_Surprised_Pikachu_HD.jpg"
+              alt="Profile Image"
+              width={150}
+              height={150}
+              className="rounded-full mt-2"
+            />
+          </div>
+          <button
+            onClick={handleDelete}
+            className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded"
+          >
+            Delete Account
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
