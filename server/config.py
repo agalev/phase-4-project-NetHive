b79894662d130c9d959c32cb1ab60b03cf394256
Alexander <alex.galev@yahoo.com>
2023-04-17 10:14:20 -0600

    Co-authored-by: sdbarlow <sethdbarlow@gmail.com>

--- server/config.py
+++ server/config.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
@@ -6,11 +8,14 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+load_dotenv()
+
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.secret_key = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+
 
 metadata = MetaData(naming_convention={
     "ix": 'ix_%(column_0_label)s',
@@ -19,6 +24,7 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 })
+
 
 db = SQLAlchemy(metadata=metadata)
 
