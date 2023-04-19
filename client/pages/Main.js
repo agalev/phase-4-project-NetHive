f7cf5f2a64fef37c60e17cecbe31aa46b9914065
Alexander <alex.galev@yahoo.com>
2023-04-19 15:18:44 -0600

    sidebar

--- client/pages/Main.js
+++ client/pages/Main.js
@@ -2,17 +2,11 @@ import React from 'react'
 import Header from '../components/Header'
 import MessageDisplay from '../components/MessageDisplay'
 import SideBar from '../components/SideBar'
-import { useSelector } from 'react-redux'
 
 function Main() {
-  const loggedUser = useSelector((state) => state.user);
-  
-  // console.log('loggedUser', loggedUser.user.image)
-  // const id = loggedUser.user.id
-  // if (loggedUser.isLoggedIn) {console.log('id', loggedUser.user.id)}
 	return (
 		<>
-			<Header loggedUser={loggedUser}/>
+			<Header />
 			<div className='flex'>
 				<div className='w-1/4'>
 					<SideBar />
